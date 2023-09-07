176aad360f182c62d62936db77c9a09a3c9f2aac
SriDurgaPrasad <119719304+SriDurgaPrasad@users.noreply.github.com>
2023-09-07 16:02:07 +0530

    def rev(m):     s=0     while m!= 0:         r=m%10         s=s*10+r         m=m//10     return s def ans(m) m=n**2 r=rev(n) d=r**2 e=rev(d) if(m==e):     return True else:     return False n=int(input()) ans(m)

---
+++ Adam_number.py
@@ -0,0 +1,18 @@
+def rev(m):
+    s=0
+    while m!= 0:
+        r=m%10
+        s=s*10+r
+        m=m//10
+    return s
+def ans(m)
+m=n**2
+r=rev(n)
+d=r**2
+e=rev(d)
+if(m==e):
+    return True
+else:
+    return False
+n=int(input())
+ans(m)
