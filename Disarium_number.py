d7d0aa7fba0775dfb7eb4cb630009211cda1e2b9
SriDurgaPrasad <119719304+SriDurgaPrasad@users.noreply.github.com>
2023-09-08 21:08:24 +0530

    n=int(input()) s=0 t=n l=int(len(str(n))) while n != 0:     r=n%10     s=s+r**l     l=l-1     n=n//10 if t==s:     print(True) else:     print(False)

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,14 +1,13 @@
 n=int(input())
-t=n
 s=0
+t=n
 l=int(len(str(n)))
-while n:
+while n != 0:
     r=n%10
     s=s+r**l
     l=l-1
     n=n//10
-if s==t:
+if t==s:
     print(True)
 else:
     print(False)
-    
